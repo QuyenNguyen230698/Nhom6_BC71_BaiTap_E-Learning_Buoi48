af3f028731b6ca09e0117685e3471fb892e16d41
Quyen <at128ve@gmail.com>
2024-11-29 09:46:59 +0700

    create class index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div className='flex flex-row gap-4 w-full'>
-      <div><button><NavLink to="/">Home</NavLink></button></div>
-      <div><button><NavLink to="/login">Login</NavLink></button></div>
-      <div><button><NavLink to="/signup">Signup</NavLink></button></div>
-      <div><button><NavLink to="/account">Account</NavLink></button></div>
-    </div>
+    <header className='flex flex-row gap-4 w-full'>
+      <div className='textBanner'><button><NavLink to="/">Home</NavLink></button></div>
+      <div className='textSubBanner'><button><NavLink to="/login">Login</NavLink></button></div>
+      <div className='textTitle'><button><NavLink to="/signup">Signup</NavLink></button></div>
+      <div className='textSubTitle'><button><NavLink to="/account">Account</NavLink></button></div>
+    </header>
   )
 }
