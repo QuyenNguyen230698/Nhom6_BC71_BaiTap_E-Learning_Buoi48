42446526e140ada0e1702a96d144e3c91847ee57
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2024-12-13 13:39:33 +0700

    update localePath

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,6 +56,10 @@ export default function Header() {
       ))}
     </>
   );
+  // Điều hướng tới trang account hoặc login
+  function getNavLinkPath() {
+    return localStorage.getItem('DATA_USER') ? '/account' : '/login';
+  }
 
   return (
     <div className='h-full w-full'>
@@ -91,7 +95,7 @@ export default function Header() {
           </div>
           <div data-aos="fade-down" data-aos-delay="100" className="navbar-end w-auto flex flex-row items-end gap-4">
             <div className='w-fit'>
-              <NavLink to="/account">
+              <NavLink to={getNavLinkPath()}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
@@ -141,7 +145,7 @@ export default function Header() {
               </label>
             </div>
             <div className='w-fit'>
-              <NavLink to="/account">
+              <NavLink to={getNavLinkPath()}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
--- src/pages/accounts/Account.jsx
+++ src/pages/accounts/Account.jsx
@@ -2,25 +2,32 @@ import React, { useEffect, useState } from 'react'
 import { adminService } from '../../../services/Vlearning'
 import { useDispatch } from 'react-redux'
 import { turnOffLoading } from '../../redux/loadingSlice'
+import { message } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
 export default function Account() {
   const [users, setUsers] = useState({})
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   useEffect(() => {
     let dataUser = JSON.parse(localStorage.getItem("DATA_USER"));
-    if (dataUser) {
-      setUsers(dataUser.taiKhoan);
+    setUsers(dataUser)
+    if (!dataUser) {
+      message.warning("Vui lòng đăng nhập để sử dụng chức năng này")
+      navigate("/login")
     }
-    adminService.getUserDetailByAccount(dataUser.taiKhoan).then((result) => {
-      setUsers(result.data)
-      dispatch(turnOffLoading())
-    }).catch((err) => {
-      dispatch(turnOffLoading())
-    })
   }, [])
   return (
     <div>
-       <p> Đây là account </p>
+      <p>Đây là account</p>
+      <div>
+        <p>Email: {users.email}</p>
+        <p>Họ Tên: {users.hoTen}</p>
+        <p>Mã Loại Người Dùng: {users.maLoaiNguoiDung}</p>
+        <p>Số ĐT: {users.soDT}</p>
+        <p>Tài Khoản: {users.taiKhoan}</p>
+        <p>Tên Loại Người Dùng: {users.maLoaiNguoiDung === "HV" ? "Học Viên" : users.maLoaiNguoiDung === "GV" ? "Giáo Viên" : users.tenLoaiNguoiDung}</p>
+      </div>
     </div>
   )
 }
