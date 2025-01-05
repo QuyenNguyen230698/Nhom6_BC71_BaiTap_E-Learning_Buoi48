15360ff624f6be087b72ad86c28f9871496e7b43
lethithuha67 <lethithuhadev@gmail.com>
2025-01-05 14:42:35 +0700

    update gắn link footer

--- src/components/ErrorPage/Error.jsx
+++ src/components/ErrorPage/Error.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 const Error = ({ code = "Error", message = "Something went wrong" }) => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
   return (
     <div className="flex flex-col container mx-auto max-w-4xl w-full h-full min-h-screen bg-home items-center justify-center gap-2">
       <h1 className="text-black-gray text-4xl md:text-6xl font-bold">{code}</h1>
       <p className="text-gray-600 test-lg">{message}</p>
-      <a href="/" className="btn btn-error text-center">
+      <a onClick={scrollToTop} href="/" className="btn btn-error text-center">
         Go back to Home
       </a>
     </div>
   );
 };
+
+
 
 export default Error;
